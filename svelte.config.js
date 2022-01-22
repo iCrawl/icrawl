673ffe1bfddf214819f0f18fe9e39d684aeeee61
iCrawl <buechler.noel@outlook.com>
2022-01-22 21:10:15 +0100

    chore: use adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import WindiCSS from 'vite-plugin-windicss';
 
@@ -9,7 +9,7 @@ const config = {
 	preprocess: [preprocess()],
 
 	kit: {
-		adapter: adapter({ out: 'dist' }),
+		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
