30b38114b2f6548030c0bc4c3fc107d5bb88e70e
iCrawl <buechler.noel@outlook.com>
2023-05-01 11:05:42 +0200

    build: pre-commit

--- e2e/basic.spec.ts
+++ e2e/basic.spec.ts
@@ -1,7 +1,13 @@
 import { expect, test } from "@playwright/test";
 
-test("index page has expected headings", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
 	await page.goto("/");
-	expect(await page.textContent("h1")).toBe("Hey, I'm Crawl!");
-	expect(await page.textContent("h2")).toBe("Software Developer");
+});
+
+test.describe("Visit home", () => {
+	test("/", async ({ page }) => {
+		await page.goto("/");
+		expect(await page.getByRole("heading", { level: 1 }).textContent()).toBe("Hey, I'm Crawl!");
+		expect(await page.getByRole("heading", { level: 2 }).textContent()).toBe("Software Developer");
+	});
 });
